b1a2fbf021f7ae0c0f688ab06efc00cd7065c76a
JulienGrade <gradejulien@gmail.com>
2024-03-07 15:44:16 +0100

    Delete js config for Vercel

--- build-script.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-const configContent = `
-const config = {
-  API_URL: "${process.env.API_URL}",
-  API_KEY: "${process.env.API_KEY}"
-};
-`;
-fs.writeFileSync('./public/config.js', configContent);
