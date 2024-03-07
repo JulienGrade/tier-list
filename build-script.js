68a85beafd2051899eb3cc327934b207763765fd
JulienGrade <gradejulien@gmail.com>
2024-03-07 15:12:07 +0100

    Fix build script

--- build-script.js
+++ build-script.js
@@ -1,9 +1,8 @@
 const fs = require('fs');
-const path = require('path');
-
-const configContent = `const config = {
+const configContent = `
+const config = {
   API_URL: "${process.env.API_URL}",
   API_KEY: "${process.env.API_KEY}"
-};`;
-
-fs.writeFileSync(path.join(__dirname, 'public','config.js'), configContent);
+};
+`;
+fs.writeFileSync('./public/config.js', configContent);
